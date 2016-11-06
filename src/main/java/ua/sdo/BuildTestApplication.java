318b44349a63867fa8bb2c0fc4a68f80c0d38faf
dimsdale <dimmsdale93@gmail.com>
2016-11-06 14:20:45 +0200

    Initial commit

---
+++ src/main/java/ua/sdo/BuildTestApplication.java
@@ -0,0 +1,12 @@
+package ua.sdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildTestApplication.class, args);
+	}
+}
